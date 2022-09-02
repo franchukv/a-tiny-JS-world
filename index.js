885809c882776dfe699c5e399bdf81fc184979d6
Volodymyr Franchuk <franchuk.vldr@gmail.com>
2022-09-02 12:55:04 +0300

    Finished 'Tiny JS World' task

--- index.js
+++ index.js
@@ -7,7 +7,59 @@
 
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
+const man = {
+   species: 'human',
+   gender: 'male',
+   name: 'Drogo',
+   status: 'Khal',
+   hands: 2,
+   feets: 2,
+   saying: 'Sheikh Ma Shieraki Anni.',
+}
 
+const woman = {
+   species: 'human',
+   gender: 'female',
+   name: 'Daenerys',
+   status: 'Daenerys of House Targaryen, First with Her Name, Breaker of Chains and Mother of Dragons',
+   hands: 2,
+   feets: 2,
+   saying: 'Drakaris!',
+   friends: `Fell in love with ${man.name}.`,
+}
+
+const dog = {
+   species: 'dog',
+   gender: 'male',
+   name: 'Sharik',
+   status: "Yard's terrier",
+   hands: 0,
+   feets: 4,
+   saying: 'woof-woof!',
+   friends: `Only ${man.name} is friends.`,
+}
+
+const cat = {
+   species: 'cat',
+   gender: 'female',
+   name: 'Bastet',
+   status: "Pharaoh's cat",
+   hands: 0,
+   feets: 4,
+   saying: 'Meow, bow to me!',
+   friends: 'No friends, only servants!'
+}
+
+const catwoman = {
+   species: 'human',
+   gender: 'female',
+   name: 'Selina Kyle',
+   status: "Superhero",
+   hands: 2,
+   feets: 2,
+   saying: cat.saying,
+   friends: `In astral connection with ${cat.name}.`,
+}
 
 // ======== OUTPUT ========
 /* Use print(message) for output.
@@ -27,5 +79,23 @@
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
    */
+const inhabitants = [man, woman, dog, cat, catwoman];
 
+const objToString = (obj) => {
+   const propNames = Object.getOwnPropertyNames(obj);
+   let propValuesArr = [];
 
+   for (let i = 0; i < propNames.length; i++) {
+      propValuesArr.push(obj[propNames[i]]);
+   }
+
+   return propValuesArr.join('; ');
+}
+
+function greetings(arr, func) {
+   for (let i = 0; i < arr.length; i++) {
+      print(func(arr[i]))
+   }
+}
+
+greetings(inhabitants, objToString)
