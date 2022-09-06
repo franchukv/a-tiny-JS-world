a3baa9dc3ace833c286c027f161d08ece3bab911
Volodymyr Franchuk <franchuk.vldr@gmail.com>
2022-09-06 10:10:41 +0300

    Finished 'OOP exercise' module

--- index.js
+++ index.js
@@ -1,101 +1,87 @@
-/* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
+/* 
+   Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
    Complete the below for code reviewers' convenience:
 
-   Code repository: _put repo URL here_
-   Web app: _put project's github pages URL here_
-   */
+   Code repository: https://github.com/franchukv/a-tiny-JS-world
+   Web app: https://franchukv.github.io/a-tiny-JS-world/
+*/
 
 // ======== OBJECTS DEFINITIONS ========
-// Define your objects here
-const man = {
-   species: 'human',
-   gender: 'male',
-   name: 'Drogo',
-   status: 'Khal',
-   hands: 2,
-   feets: 2,
-   saying: 'Sheikh Ma Shieraki Anni.',
+class Inhabitants {
+   constructor(species, gender, name, status, saying, friends) {
+      this.species = species;
+      this.gender = gender;
+      this.name = name;
+      this.status = status;
+      this.saying = saying;
+      this.friends = friends;
+      this.properties = ['species', 'gender', 'name', 'status'];
+   }
+
+   objToString() {
+      return this.properties.map((property) => this[property]).join('; ');
+   }
+
+   toMakeFriends() {
+      return this.properties.push('friends');
+   }
+
 }
 
-const woman = {
-   species: 'human',
-   gender: 'female',
-   name: 'Daenerys',
-   status: 'Daenerys of House Targaryen, First with Her Name, Breaker of Chains and Mother of Dragons',
-   hands: 2,
-   feets: 2,
-   saying: 'Drakaris!',
-   friends: `Fell in love with ${man.name}.`,
+class Human extends Inhabitants {
+   constructor(species, gender, name, status, saying, friends) {
+      super(species, gender, name, status, saying, friends);
+      this.hands = 2;
+      this.feets = 2;
+      this.properties = [...this.properties, 'hands', 'feets', 'saying'];
+   }
 }
 
-const dog = {
-   species: 'dog',
-   gender: 'male',
-   name: 'Sharik',
-   status: "Yard's terrier",
-   hands: 0,
-   feets: 4,
-   saying: 'woof-woof!',
-   friends: `Only ${man.name} is friends.`,
+class Animal extends Inhabitants {
+   constructor(species, gender, name, status, saying, friends) {
+      super(species, gender, name, status, saying, friends);
+      this.feets = 4;
+      this.properties = [...this.properties, 'feets', 'saying'];
+   }
 }
 
-const cat = {
-   species: 'cat',
-   gender: 'female',
-   name: 'Bastet',
-   status: "Pharaoh's cat",
-   hands: 0,
-   feets: 4,
-   saying: 'Meow, bow to me!',
-   friends: 'No friends, only servants!'
+class Man extends Human {
+   constructor(name, status, saying, friends) {
+      super('human', 'male', name, status, saying, friends);
+   }
 }
 
-const catwoman = {
-   species: 'human',
-   gender: 'female',
-   name: 'Selina Kyle',
-   status: "Superhero",
-   hands: 2,
-   feets: 2,
-   saying: cat.saying,
-   friends: `In astral connection with ${cat.name}.`,
+class Woman extends Human {
+   constructor(name, status, saying, friends) {
+      super('human', 'female', name, status, saying, friends);
+   }
 }
+
+class Dog extends Animal {
+   constructor(gender, name, status, saying, friends) {
+      super('dog', gender, name, status, saying, friends);
+   }
+}
+
+class Cat extends Animal {
+   constructor(gender, name, status, saying, friends) {
+      super('cat', gender, name, status, saying, friends);
+   }
+}
+
+const man = new Man('Drogo', 'Khal', 'Sheikh Ma Shieraki Anni.');
+const woman = new Woman('Daenerys', 'Daenerys of House Targaryen, First with Her Name, Breaker of Chains and Mother of Dragons', 'Drakaris!', `Fell in love with ${man.name}.`);
+const dog = new Dog('male', 'Sharik', "Yard's terrier", 'woof-woof!', `Only ${man.name} is friends.`);
+const cat = new Cat('female', 'Bastet', "Pharaoh's cat", 'Meow, bow to me!', 'No friends, only servants!');
+const catwoman = new Woman('Selina Kyle', 'Superhero', cat.saying, `In astral connection with ${cat.name}.`);
 
 // ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
+const allInhabitants = [
+   man.objToString(),
+   woman.objToString(woman.toMakeFriends()),
+   dog.objToString(dog.toMakeFriends()),
+   cat.objToString(cat.toMakeFriends()),
+   catwoman.objToString(catwoman.toMakeFriends()),
+];
 
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
-
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
-const inhabitants = [man, woman, dog, cat, catwoman];
-
-const objToString = (obj) => {
-   const propNames = Object.getOwnPropertyNames(obj);
-   let propValuesArr = [];
-
-   for (let i = 0; i < propNames.length; i++) {
-      propValuesArr.push(obj[propNames[i]]);
-   }
-
-   return propValuesArr.join('; ');
-}
-
-function greetings(arr, func) {
-   for (let i = 0; i < arr.length; i++) {
-      print(func(arr[i]))
-   }
-}
-
-greetings(inhabitants, objToString)
+allInhabitants.forEach((inhabitant) => print(inhabitant));
